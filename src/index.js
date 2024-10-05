c39f2fc004f7b38511a9c5340f5bb51fd8f50917
Serkan <serkankaragoz2002@gmail.com>
2024-10-06 00:29:02 +0300

    updated. package.json This is just testing hase btw. The actual website will come soon

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from "react-router-dom";
+
+import { HashRouter } from 'react-router-dom';;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <React.StrictMode>
+    <HashRouter>
       <App />
-  </Router>
+    </HashRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
