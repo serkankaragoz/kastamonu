f55b370774c26031311cf8a2da61bbd55e863e4c
Serkan <serkankaragoz2002@gmail.com>
2024-10-06 00:35:03 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
+import './styles/index.css';
+import Page1 from './components/Page1/Page1';
+import DynamicPage from './components/DynamicPage/DynamicPage';
 
-import { HashRouter } from 'react-router-dom';;
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <div>haloo</div>,
+  },{
+    path: "/pag",
+    element: <div>HMMMMM</div>,
+  },{
+    path: "/page/1",
+    element: <Page1 />,
+  },{
+    path: "/pages",
+    element: <DynamicPage />,
+  }
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
